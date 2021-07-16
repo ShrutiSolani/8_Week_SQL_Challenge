2d68166308b074abf75ff77398b7a881909e1587
ShrutiSolani <68545365+ShrutiSolani@users.noreply.github.com>
2021-07-16 20:51:22 +0530

    Update queries.sql

--- Case Study 1/queries.sql
+++ Case Study 1/queries.sql
@@ -106,3 +106,90 @@
 ## Query 6
 -- 6. Which item was purchased first by the customer after they became a member?
 
+    WITH RANKED AS(
+    	SELECT RANK() OVER(PARTITION BY DANNYS_DINER.SALES.CUSTOMER_ID ORDER BY ORDER_DATE) AS RANKING, DANNYS_DINER.SALES.CUSTOMER_ID, PRODUCT_ID, ORDER_DATE
+        FROM DANNYS_DINER.SALES, DANNYS_DINER.MEMBERS
+        WHERE DANNYS_DINER.SALES.CUSTOMER_ID = DANNYS_DINER.MEMBERS.CUSTOMER_ID AND DANNYS_DINER.SALES.ORDER_DATE >= DANNYS_DINER.MEMBERS.JOIN_DATE 
+      )
+    
+    SELECT RANKED.CUSTOMER_ID, DANNYS_DINER.MENU.PRODUCT_NAME
+    FROM ranked, DANNYS_DINER.MENU
+    WHERE ranking = 1 AND RANKED.PRODUCT_ID = DANNYS_DINER.MENU.PRODUCT_ID;
+
+| customer_id | product_name |
+| ----------- | ------------ |
+| B           | sushi        |
+| A           | curry        |
+
+---
+
+## Query 7
+-- 7. Which item was purchased just before the customer became a member?
+
+    WITH RANKED AS(
+    	SELECT RANK() OVER(PARTITION BY DANNYS_DINER.SALES.CUSTOMER_ID ORDER BY ORDER_DATE DESC) AS RANKING, DANNYS_DINER.SALES.CUSTOMER_ID, PRODUCT_ID, ORDER_DATE
+        FROM DANNYS_DINER.SALES, DANNYS_DINER.MEMBERS
+        WHERE DANNYS_DINER.SALES.CUSTOMER_ID = DANNYS_DINER.MEMBERS.CUSTOMER_ID AND DANNYS_DINER.SALES.ORDER_DATE < DANNYS_DINER.MEMBERS.JOIN_DATE 
+      )
+    
+    SELECT RANKED.CUSTOMER_ID, DANNYS_DINER.MENU.PRODUCT_NAME
+    FROM ranked, DANNYS_DINER.MENU
+    WHERE ranking = 1 AND RANKED.PRODUCT_ID = DANNYS_DINER.MENU.PRODUCT_ID;
+
+| customer_id | product_name |
+| ----------- | ------------ |
+| B           | sushi        |
+| A           | sushi        |
+| A           | curry        |
+
+---
+
+## Query 8
+-- 8. What is the total items and amount spent for each member before they became a member?
+
+    WITH RANKED AS(
+    	SELECT RANK() OVER(PARTITION BY DANNYS_DINER.SALES.CUSTOMER_ID ORDER BY ORDER_DATE DESC) AS RANKING, DANNYS_DINER.SALES.CUSTOMER_ID, PRODUCT_ID, ORDER_DATE
+        FROM DANNYS_DINER.SALES, DANNYS_DINER.MEMBERS
+        WHERE DANNYS_DINER.SALES.CUSTOMER_ID = DANNYS_DINER.MEMBERS.CUSTOMER_ID AND DANNYS_DINER.SALES.ORDER_DATE < DANNYS_DINER.MEMBERS.JOIN_DATE 
+      )
+    
+    SELECT RANKED.CUSTOMER_ID, COUNT(RANKED.RANKING), SUM(PRICE)
+    FROM ranked, DANNYS_DINER.MENU
+    WHERE RANKED.PRODUCT_ID = DANNYS_DINER.MENU.PRODUCT_ID
+    GROUP BY RANKED.CUSTOMER_ID;
+
+| customer_id | count | sum |
+| ----------- | ----- | --- |
+| B           | 3     | 40  |
+| A           | 2     | 25  |
+
+---
+
+## Query 9
+-- 9.  If each $1 spent equates to 10 points and sushi has a 2x points multiplier - how many points would each customer have?
+
+    WITH POINTS AS (
+      SELECT *,
+      CASE WHEN DANNYS_DINER.MENU.PRODUCT_NAME = 'sushi' THEN 2*DANNYS_DINER.MENU.PRICE
+      ELSE DANNYS_DINER.MENU.PRICE 
+      END AS PT
+      FROM DANNYS_DINER.SALES, DANNYS_DINER.MENU
+      WHERE DANNYS_DINER.SALES.PRODUCT_ID = DANNYS_DINER.MENU.PRODUCT_ID
+    )
+    
+    SELECT CUSTOMER_ID, SUM(PT) AS POINTS
+    FROM POINTS
+    GROUP BY CUSTOMER_ID;
+
+| customer_id | points |
+| ----------- | ------ |
+| B           | 94     |
+| C           | 36     |
+| A           | 86     |
+
+---
+
+## Query 10
+-- 10. In the first week after a customer joins the program (including their join date) they earn 2x points on all items, 
+-- not just sushi - how many points do customer A and B have at the end of January?
+
