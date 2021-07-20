b7c07727ec78dd75079a8f863a2c307aaa64b67e
ShrutiSolani <68545365+ShrutiSolani@users.noreply.github.com>
2021-07-20 15:09:03 +0530

    Update pizza_metrics.sql

--- Case Study 2/pizza_metrics.sql
+++ Case Study 2/pizza_metrics.sql
@@ -24,3 +24,15 @@
 -- Query 3
 -- How many successful orders were delivered by each runner?
 
+    SELECT PIZZA_RUNNER.RUNNER_ORDERS.RUNNER_ID, COUNT(*) AS ORDERS
+    FROM PIZZA_RUNNER.RUNNER_ORDERS
+    WHERE PIZZA_RUNNER.RUNNER_ORDERS.CANCELLATION IS NULL
+    GROUP BY PIZZA_RUNNER.RUNNER_ORDERS.RUNNER_ID;
+
+| runner_id | orders |
+| --------- | ------ |
+| 1         | 4      |
+| 2         | 3      |
+| 3         | 1      |
+
+---
